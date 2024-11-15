ccdaaf08518109d86ef2cd013e24e1b33ef5e310
Clara Weyer <clara.weyer@schullv.de>
2024-11-15 10:33:46 +0100

    add toggle solution

--- toggle.js
+++ toggle.js
@@ -3,3 +3,10 @@ const bookmark = document.querySelector('[data-js="card__bookmark"]');
 bookmark.addEventListener("click", () => {
   bookmark.classList.toggle("card__bookmark--selected");
 });
+
+const showButton = document.querySelector('[data-js="button"]');
+const solution = document.querySelector('[data-js="solution"]');
+
+showButton.addEventListener("click", () => {
+  solution.toggleAttribute("hidden");
+});
